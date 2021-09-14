100dad415c164fe86ebd8817f4db6819335b9f82
kandarp dangi <kandarpdangi@gmail.com>
2021-09-14 22:02:06 +0530

    middleware & layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,25 +1,62 @@
-import React from "react";
-import HeadTags from "./HeadTags";
-import Navbar from "./Navbar";
-import { Container } from "semantic-ui-react";
-import nprogress from "nprogress";
-import Router from "next/router";
+import React, { createRef } from 'react'
+import HeadTags from './HeadTags'
+import Navbar from './Navbar'
+import {
+	Container,
+	Visibility,
+	Grid,
+	Sticky,
+	Ref,
+	Divider,
+	Segment,
+} from 'semantic-ui-react'
+import nprogress from 'nprogress'
+import Router from 'next/router'
+import SideMenu from './SideMenu'
+import Search from './Search'
 
-function Layout({ children }) {
-  Router.onRouteChangeStart = () => nprogress.start();
-  Router.onRouteChangeComplete = () => nprogress.done();
-  Router.onRouteChangeError = () => nprogress.done();
+function Layout({ children, user }) {
+	const contextRef = createRef()
 
-  return (
-    <>
-      <HeadTags />
-      <Navbar />
+	Router.onRouteChangeStart = () => nprogress.start()
+	Router.onRouteChangeComplete = () => nprogress.done()
+	Router.onRouteChangeError = () => nprogress.done()
 
-      <Container text style={{ paddingTop: "1rem" }}>
-        {children}
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<HeadTags />
+			{user ? (
+				<div style={{ marginLeft: '1rem', marginRight: '1rem' }}>
+					<Ref innerRef={contextRef}>
+						<Grid>
+							<Grid.Column floated='left' width={2}>
+								<Sticky context={contextRef}>
+									<SideMenu user={user} />
+								</Sticky>
+							</Grid.Column>
+
+							<Grid.Column width={10}>
+								<Visibility context={contextRef}>{children}</Visibility>
+							</Grid.Column>
+
+							<Grid.Column floated='left' width={4}>
+								<Sticky context={contextRef}>
+									<Segment basic>
+										<Search />
+									</Segment>
+								</Sticky>
+							</Grid.Column>
+						</Grid>
+					</Ref>
+				</div>
+			) : (
+				<>
+					<Navbar />
+					<Container style={{ paddingTop: '1rem' }}>{children}</Container>
+				</>
+			)}
+		</>
+	)
 }
 
-export default Layout;
+export default Layout
